4014f2f6fa854f0b3304061db6f725ae3c211673
Qasim Khan <qasim@dubizzle.com>
2014-01-06 16:32:07 +0400

    postgres_failover: added actions for master and slave

--- lib/nerve/leader_watcher/postgresql.rb
+++ lib/nerve/leader_watcher/postgresql.rb
@@ -1,17 +1,24 @@
 require 'zk'
 
 module Nerve
-	module LeaderWatcher
-	  class PostgreSQLWatcher < BaseWatcher
-	    include Logging
-	    
-	    private
-	    def action
-	      log.info("Am I master? #{master?}")
-	    end
-	  end
-
-	  WATCHERS ||= {}
-	  WATCHERS['postgresql'] = PostgreSQLWatcher
+  module LeaderWatcher
+    class PostgreSQLWatcher < BaseWatcher
+      include Logging
+      
+      private
+      def action
+        log.info("Am I master? #{master?}")
+        log.info("Hello! #{master_node}")
+        if master?
+          res = `sudo /bin/su - postgres -c /opt/smartstack/nerve/postgres_master_config.sh`
+        else
+          res = `sudo su - postgres -c "/opt/smartstack/nerve/postgres_slave_config.sh #{master_node["host"]} #{master_node["port"]}"`
+        end
+        log.info(res)
+      end
+    end
+    
+    WATCHERS ||= {}
+    WATCHERS['postgresql'] = PostgreSQLWatcher
   end
 end
