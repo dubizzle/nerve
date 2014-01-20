6062b919dc774f775368c41694c26e43c999ebe5
danny crasto <danny@dubizzle.com>
2014-01-20 15:54:41 +0400

    added email and thread_pool classes
    to send email notifications
    thread_pool (found online) manages email threads
    
    Signed-off-by: danny crasto <danny@dubizzle.com>

---
+++ lib/nerve/email.rb
@@ -0,0 +1,34 @@
+require 'mail'
+require_relative 'thread_pool.rb'
+
+module Nerve
+  class Email
+  include Logging
+    def initialize()
+      @pool = Pool.new(10)
+
+    end
+
+    def send_email(subject,
+                   message,
+                   address_from = 'watcher@dubizzle.com',
+                   address_to = 'seb@dubizzle.com,qasim@dubizzle.com,aamir@dubizzle.com')
+    
+        @pool.schedule do
+          body = "From: #{address_from}\n\
+                  Subject: #{subject}\n\
+                  #{message}"
+          result = `echo "#{body}" | sendmail "#{address_to}"`
+          if x != 0
+            log.error("Failed to send email")
+          else
+            log.info("Notification email sent to recipients")
+        end
+    end
+
+    def finailize()
+      at_exit { @pool.shutdown }
+      log.info('Done with email notify')
+    end
+  end
+end
