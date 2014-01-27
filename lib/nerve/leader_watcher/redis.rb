6435a732ee6e9716480f798e8e624c4056a43723
Qasim Khan <qasim@dubizzle.com>
2014-01-27 19:09:41 +0400

    redis-failover: notify and interval logic added into the redis watcher

--- lib/nerve/leader_watcher/redis.rb
+++ lib/nerve/leader_watcher/redis.rb
@@ -1,31 +1,30 @@
+require_relative 'common'
+
 module Nerve
   module LeaderWatcher
     class RedisWatcher < BaseWatcher
       include Logging
 
-      def initialize(opts={})
-        %w{hosts path host port service_home}.each do |required|
-          raise ArgumentError, "you need to specify required argument #{required}" unless opts[required]
-        end
-
-        @hosts = opts['hosts']
-        @path = opts['path']
-        @host = opts['host']
-        @port = opts['port']
-        @service_home = opts['service_home']
-      end
-
       private
       def action
         log.info("Am I master? #{master?}")
-        if master?
-          command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_master_config.sh #{@service_home} #{@host} #{@port}'"
-        else
-          command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_slave_config.sh #{@service_home} #{master_node["host"]} #{master_node["port"]} #{@host} #{@port}'"
+        
+        if !failover
+            log.info("Not failing over")
+            notify("Not Failing over", "Failover time less than interval. Something is wrong!")
+            return
         end
-        log.debug "#{command}"
-        res = `#{command}`
-        log.info("redis response #{res}")
+
+        if new_node_state != StatusChange::NO_CHANGE
+          if new_node_state == StatusChange::PROMOTED
+            command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_master_config.sh #{@service_home} #{@host} #{@port}'"
+          else
+            command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_slave_config.sh #{@service_home} #{master_node["host"]} #{master_node["port"]} #{@host} #{@port}'"
+          end
+          log.debug "#{command}"
+          res = `#{command}`
+          log.info("redis response #{res}")
+        end
       end
     end
 
