c87b69d1ec9207d46b2a8b8f1e433814ffc3d0ea
Qasim Khan <qasim@dubizzle.com>
2014-01-28 16:14:20 +0400

    redis-failover: got redis failover working

--- lib/nerve/leader_watcher/redis.rb
+++ lib/nerve/leader_watcher/redis.rb
@@ -7,22 +7,25 @@ module Nerve
 
       private
       def action
-        log.info("Am I master? #{master?}")
-        
-        if !failover
+        state = previous_state
+        if terminate_failover(state)
             log.info("Not failing over")
             notify("Not Failing over", "Failover time less than interval. Something is wrong!")
             return
         end
 
+        new_node_state = node_state_update(state)
         if new_node_state != StatusChange::NO_CHANGE
           if new_node_state == StatusChange::PROMOTED
+            ss = 'master'
             command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_master_config.sh #{@service_home} #{@host} #{@port}'"
           else
+            ss = 'slave'
             command = "sudo su - vagrant -c  '/opt/smartstack/nerve/redis_slave_config.sh #{@service_home} #{master_node["host"]} #{master_node["port"]} #{@host} #{@port}'"
           end
-          log.debug "#{command}"
+          log.info "---> Previous State #{state} Is master? #{master?} Port: #{@port} Status: #{new_node_state} Command: #{command}"
           res = `#{command}`
+          aa = `echo #{ss} > /tmp/#{@host}_#{@port}.state && echo '#{@host} #{@port} #{Time.now.to_f} #{ss}' >> /tmp/status.log`
           log.info("redis response #{res}")
         end
       end
