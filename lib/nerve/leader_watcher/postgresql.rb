134e929c82da2db6196326a593a0fbab87b35e4e
Qasim Khan <qasim@dubizzle.com>
2014-03-12 10:20:48 +0400

    notify: fixing postgres failover

--- lib/nerve/leader_watcher/postgresql.rb
+++ lib/nerve/leader_watcher/postgresql.rb
@@ -4,30 +4,33 @@ module Nerve
   module LeaderWatcher
     class PostgreSQLWatcher < BaseWatcher
       include Logging
-      
+
       private
       def action
-        log.info("Am I master? #{master?}")
-        log.info("Hello! #{master_node}")
-  
-        if !failover
+        state = previous_state
+        if terminate_failover(state)
             log.info("Not failing over")
             notify("Not Failing over", "Failover time less than interval. Something is wrong!")
             return
         end
 
+        new_node_state = node_state_update(state)
         if new_node_state != StatusChange::NO_CHANGE
           if new_node_state == StatusChange::PROMOTED
-            res = `sudo /bin/su - postgres -c /opt/smartstack/nerve/postgres_master_config.sh`
+            ss = 'master'
+            command = "sudo /bin/su - postgres -c /opt/smartstack/nerve/postgres_master_config.sh'"
           else
-            res = `sudo su - postgres -c "/opt/smartstack/nerve/postgres_slave_config.sh #{master_node["host"]} #{master_node["port"]}"`
+            ss = 'slave'
+            command = "sudo su - postgres -c '/opt/smartstack/nerve/postgres_slave_config.sh #{master_node["host"]} #{master_node["port"]}'"
           end
-          log.info(res)
+          log.info "---> Previous State #{state} Is master? #{master?} Port: #{@port} Status: #{new_node_state} Command: #{command}"
+          res = `#{command}`
+          aa = `echo #{ss} > /tmp/#{@host}_#{@port}.state && echo '#{@host} #{@port} #{Time.now.to_f} #{ss}' >> /tmp/status.log`
+          log.info("redis response #{res}")
         end
       end
+    end
 
-   end
-    
     WATCHERS ||= {}
     WATCHERS['postgresql'] = PostgreSQLWatcher
   end
